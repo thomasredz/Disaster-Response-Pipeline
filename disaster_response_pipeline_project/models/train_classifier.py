77cc58c8b2e08336f8ec8206f6038259a95ff3bc
thomasredz <67224679+thomasredz@users.noreply.github.com>
2022-06-08 16:37:03 +0200

    almost finished

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -1,24 +1,72 @@
 import sys
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+import pickle
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('InsertTableName', engine)  
+    X = df.message
+    Y = df.drop(columns=["id", "message", "original", "genre"])
+    category_names = list(df.columns[4:])
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # Normalize
+    text = text.lower().strip()
+    text = re.sub(r"[^a-z0-9]", " ", text)
+    # Tokenize
+    token = word_tokenize(text)
+    # Remove stop words
+    filtered_words = [word for word in token if word not in stopwords.words('english')]
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(word) for word in filtered_words]
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    accuracy = (y_pred == y_test.values).mean()
+    print(classification_report(y_test.iloc[:, 1:].values, np.array([x[1:] for x in y_pred])))#, target_names = category_names))
+    print('The model accuracy is {:.3f}'.format(accuracy))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open (model_filepath, 'wb') as f:
+        pickle.dump(model, f)  
 
 
 def main():

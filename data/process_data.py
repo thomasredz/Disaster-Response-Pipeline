b280b474c8cf664ecef122dd0ddd9808c9be9fc7
thomasredz <67224679+thomasredz@users.noreply.github.com>
2022-06-15 16:36:23 +0200

    modified in order to add the table if already exists

--- data/process_data.py
+++ data/process_data.py
@@ -59,7 +59,7 @@ def save_data(df, database_filename):
     Returns:
     """
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('disaster_msg_tbl', engine, index=False)  
+    df.to_sql('disaster_msg_tbl', engine, index=False, if_exists='replace')  
 
 
 def main():

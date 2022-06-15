e55075c965fa31184f52f8c17bcff111d6374133
thomasredz <67224679+thomasredz@users.noreply.github.com>
2022-06-15 16:41:27 +0200

    added Gridsearch CV

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -60,14 +60,22 @@ def build_model():
     Build the model which will be used for the prediction.
     Args:
     Returns:
-        -pipeline (model): Model which need to be trained
+        -cv (model): Model which need to be trained
     """
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
-    return pipeline
+
+    parameters = {
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
